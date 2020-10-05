93084e7d363557bb20e9c23f252a1f2aeb106662
Shogo Nakashima <shogonakashima@N.Shogo>
2020-10-05 18:03:30 +0900

    new file

---
+++ BlackJack_Nakashima/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BlackJack_Nakashima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*･゜ﾟ･*:.｡..｡.:*･'ブラックジャックへようこそ'･*:.｡. .｡.:*･゜ﾟ･*" +
+                "\nゲームを開始します。" +
+                "\n");
+            Random rand = new Random();
+
+        }
+    }
+}
